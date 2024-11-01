5d22c998eef08e92f70ded215720a4472276448f
Ashutosh Dixit <ashudixitji786@gmail.com>
2024-11-01 17:38:22 +0530

    [Ashutosh]:Setup the Project with configurations

---
+++ src/main/java/com/bits/api_bp/food_del_sys_g12/food_del_sys_g12.java
@@ -0,0 +1,14 @@
+package com.bits.api_bp.food_del_sys_g12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class food_del_sys_g12 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(food_del_sys_g12.class, args);
+        System.out.println("Started food_del_sys_g12");
+    }
+
+}
---
+++ src/test/java/com/bits/api_bp/food_del_sys_g12/fooddelsysg12Tests.java
@@ -0,0 +1,13 @@
+package com.bits.api_bp.food_del_sys_g12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class fooddelsysg12Tests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
