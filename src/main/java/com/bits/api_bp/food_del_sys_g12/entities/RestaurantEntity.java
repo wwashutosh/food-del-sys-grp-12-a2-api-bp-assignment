334b8b4f52e545368f376dfba4746a73e2cdcef1
Ashutosh Dixit <ashudixitji786@gmail.com>
2024-11-11 01:20:31 +0530

    [Ashutosh]:Added test dependency and initial code for customercontroller test

--- src/main/java/com/bits/api_bp/food_del_sys_g12/entities/RestaurantEntity.java
+++ src/main/java/com/bits/api_bp/food_del_sys_g12/entities/RestaurantEntity.java
@@ -1,5 +1,6 @@
 package com.bits.api_bp.food_del_sys_g12.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
@@ -11,10 +12,15 @@ import lombok.Data;
 public class RestaurantEntity {
 
     @Id
+    @Column(name = "id", nullable = false)
     private String id;
+    @Column(name = "name", nullable = false)
     private String name;
+    @Column(name = "address", nullable = false)
     private String address;
+    @Column(name = "cuisine", nullable = false)
     private String cuisine;
+    @Column(name = "rating", nullable = false)
     private double rating;
 
 }
